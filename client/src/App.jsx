41f060be8fdc9857a0a49b0e4991f67a2c148050
gstroup11 <gstroup11@gmail.com>
2023-12-21 20:53:28 -0500

    Added in app.jsx to have an apollo provider to make all requests work with the apollo server.  I also updated the search books.jsx to use the savebook mutation and removed the savebook function from the old api file that has since been deleted.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,38 @@
+import { ApolloProvider } from '@apollo/client';
+import {
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+  ApolloLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
 import './App.css';
 import { Outlet } from 'react-router-dom';
-
 import Navbar from './components/Navbar';
+
+// Create an HTTP link
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// Create a context link with an empty headers object
+const contextLink = setContext(() => ({ headers: {} }));
+
+// Create the Apollo Client
+const client = new ApolloClient({
+  link: ApolloLink.from([contextLink, httpLink]),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
+    <ApolloProvider client={client}>
+      <div>
+        <Navbar />
+        <Outlet />
+      </div>
+    </ApolloProvider>
   );
 }
 
