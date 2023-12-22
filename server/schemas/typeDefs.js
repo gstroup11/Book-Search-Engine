927ca54568fb4e0e75f697f807d639a7537dc8bb
gstroup11 <gstroup11@gmail.com>
2023-12-21 20:21:01 -0500

    This adds in typeDefs anbd they were taken directly from the outline in the readme.

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,48 @@
+const { gql } = require('apollo-server');
+
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String!
+    email: String
+    password: String
+    savedBooks: [Book]
+    bookCount: Int
+  }
+
+  type Book {
+    bookId: ID!
+    title: String!
+    authors: [String]
+    description: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  input BookInput {
+    bookId: String!
+    title: String!
+    authors: [String]
+    description: String!
+    image: String
+    link: String
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    createUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    saveBook(book: BookInput!): User
+    deleteBook(bookId: ID!): User
+  }
+`;
+
+module.exports = typeDefs;
