5058a073f2c6bdbec8cd946ed2ed471ead6977fe
gstroup11 <gstroup11@gmail.com>
2023-12-22 13:13:42 -0500

    Fixed a bug with typedef not having user defined and gql not being needed.

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,6 +1,4 @@
-const { gql } = require('apollo-server');
-
-const typeDefs = gql`
+const typeDefs = `
   type User {
     _id: ID!
     username: String!
@@ -35,6 +33,7 @@ const typeDefs = gql`
 
   type Query {
     me: User
+    user (username: String!): User
   }
 
   type Mutation {
